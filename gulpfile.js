f3d2bfb0f99819310cc69871c4465a413a8d13cc
antonija_simic <antonija1023@gmail.com>
2015-11-01 13:02:00 +0100

    minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename');
+
 
 gulp.task('styles', function() {
-   return gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+  return sass('sass', { style: 'expanded' })
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css'));
 });
 
-gulp.task('watch', function(){
-  gulp.watch('sass/**/*.scss',['styles']);
+gulp.task('watch', function() {
+  gulp.watch('sass/*.scss', ['styles']);
 });
 
-gulp.task("default", ['styles', 'watch']);
+gulp.task("default", [ 'styles','watch']);
