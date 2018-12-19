5e7cf7a65711d44d8910d4fd236971f179a32633
Antonija Simic <antonija1023@gmail.com>
2018-12-19 14:10:20 +0100

    add gulp-clean and gulp-rename; add new scripts

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
 
-gulp.task("sass", function() {
+gulp.task("css_classes", function() {
   gulp
-    .src("./sass/*.scss")
+    .src("./sass/css_classes.scss")
     .pipe(sass())
     .pipe(
       autoprefixer({
@@ -14,3 +16,20 @@ gulp.task("sass", function() {
     )
     .pipe(gulp.dest("./css"));
 });
+
+gulp.task("minify_css", function() {
+  gulp
+    .src("./sass/css_classes.scss")
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(rename("css_classes.min.css"))
+    .pipe(gulp.dest("./css"));
+});
+
+gulp.task("default", ["minify_css"]);
