83ceeb24c432d51e82c360d89643bc4a41c7bef0
Antonija Simic <antonija1023@gmail.com>
2018-12-20 16:45:50 +0100

    show effects by groups

--- site/js/site.js
+++ site/js/site.js
@@ -95,23 +95,30 @@ const effects = [
 ];
 
 const root = $("#root");
+const arr = [];
 for (let item in effects) {
   let arrayItem = effects[item];
   let nameOfTheGroup = Object.keys(arrayItem);
   let arrayOfEffects = arrayItem[nameOfTheGroup];
+  arr.push(arrayOfEffects);
   root.append(`
-    <div class="effect-column">
-      <p> ${nameOfTheGroup} </p>
-    </div>
+    <section class="section-effect section-${nameOfTheGroup}">
+      <h3> ${nameOfTheGroup} </h3>
+    </section>
   `);
+}
 
-  for (effect in arrayOfEffects) {
-    root.append(`
-      <span id=${arrayOfEffects[effect]} class="effect"> ${
-      arrayOfEffects[effect]
-    } </span>
-    `);
-  }
+if ($(".section-effect").length > 0) {
+  $(".section-effect").each(function(i) {
+    let oneArr = arr[i];
+    let column = $(this);
+    for (let item in oneArr) {
+      console.log(oneArr[item]);
+      $(column).append(`
+       <span class="effect" id=${oneArr[item]}> ${oneArr[item]} </span>
+     `);
+    }
+  });
 }
 
 $(".effect").click(function() {
